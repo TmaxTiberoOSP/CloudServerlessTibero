028ea57afabea02e778ec297becf6e563f608cae
tmaxtibero-donghee <donghee_han2@proton.me>
2023-11-17 14:33:47 +0900

    1. DBInstance에 세부 필드 추가

--- serverless-tibero/serverless-manager/src/main/java/com/tmax/serverless/manager/context/DBInstance.java
+++ serverless-tibero/serverless-manager/src/main/java/com/tmax/serverless/manager/context/DBInstance.java
@@ -7,7 +7,12 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class DBInstance {
+  private String dbName;
   private String alias;
+  private String ip;
+  private int port;
+  private String dbUser;
+  private String dbPassword;
   private DBServerlessMode mode;
 
 }
